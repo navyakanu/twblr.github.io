970efd7790bbf3a1c486ec2ce2e80c0b75cecf2e
NavyaB <navya-balikeri@external.mckinsey.com>
2016-07-22 15:43:39 +0530

    Navya - Fixed switch tests

--- control_structures/switch_structure.go
+++ control_structures/switch_structure.go
@@ -6,5 +6,17 @@ const multiply string = "*"
 const divide = "/"
 
 func calculate(op string, arg1, arg2 float32) float32 {
+	
+	switch op {
+		case "+": return arg1+arg2
+						
+		case "-": return arg1-arg2
+					
+		case "*": return arg1*arg2
+		case "/": return arg1/arg2
+
+
+	}	
+
 	return 0.0
 }
