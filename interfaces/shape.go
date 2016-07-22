f1d79614534b14bbf8cc7ea12ba8d80b7a507a6e
NavyaB <navya-balikeri@external.mckinsey.com>
2016-07-22 23:45:05 +0530

    Navya - Added tests for interfaces

--- interfaces/shape.go
+++ interfaces/shape.go
@@ -3,3 +3,31 @@ package interfaces
 type Shape interface {
 	Area() int
 }
+
+type Square struct {
+	side int
+}
+
+type Rectangle struct {
+	length  int
+	breadth int
+}
+
+type Hybrid struct {
+	square    Square
+	rectangle Rectangle
+}
+
+func (r Rectangle) Area() int {
+	return r.length * r.breadth
+}
+
+func (s Square) Area() int {
+	return s.side * s.side
+}
+
+func (h Hybrid) Area() int {
+
+	return (h.square.Area()) + (h.rectangle.Area())
+
+}
